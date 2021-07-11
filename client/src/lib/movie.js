e73cad2ef1a2f4c8cd52709abeb1db7950a9ca44
Sean Lawlor <lawlorseanr@gmail.com>
2021-07-11 10:39:02 -0700

    Save work in progress

---
+++ client/src/lib/movie.js
@@ -0,0 +1,32 @@
+
+window.movie = {
+
+  create: (movie, callback) => {
+    axios({
+      url: '',
+      method: 'POST',
+      data: {}
+    })
+  }
+
+
+  readAll: callback => {
+
+  }
+
+
+  readOne: (id, callback) => {
+
+  }
+
+
+  update: (id, text, callback) => {
+
+  }
+
+
+  delete: (id, callback) => {
+
+  }
+
+}
--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+
 const app = express();
+var router = app.Router();
+
 const PORT = 3000 || process.env.PORT;
 
 app.use(express.static('client/dist'));
+
+
+router.
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
